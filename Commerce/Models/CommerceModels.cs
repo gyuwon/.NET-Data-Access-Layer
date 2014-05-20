1618d56ff2797ad5a0f8c4a440209f755e6f1f02
Yi Gyuwon <gyuwon@live.com>
2014-05-21 00:00:13 +0900

    CommerceModels

---
+++ Commerce/Models/CommerceModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace Commerce.Models
+{
+    public class Item
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public long Id { get; set; }
+        public long ItemId { get; set; }
+        [Required]
+        public string AuthorId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonIgnore]
+        public Item Item { get; set; }
+        [JsonIgnore]
+        public ApplicationUser Author { get; set; }
+    }
+}
