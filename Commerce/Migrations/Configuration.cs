95ff6d484c139910ba862bf6ee1f35b9200d09de
Yi Gyuwon <gyuwon@live.com>
2014-05-21 00:15:30 +0900

    Seed()

--- Commerce/Migrations/Configuration.cs
+++ Commerce/Migrations/Configuration.cs
@@ -1,31 +1,40 @@
 namespace Commerce.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
+    using Commerce.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Commerce.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Commerce.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
+            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            string administrator = "Administrator";
+            IdentityRole role = roleManager.FindByName(administrator);
+            if (role == null)
+                roleManager.Create(new IdentityRole(administrator));
+
+            UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+            UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+
+            string userName = "admin";
+            ApplicationUser user = userManager.FindByName(userName);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = userName };
+                userManager.Create(user, password: "123$%^qwe");
+            }
+
+            if (userManager.IsInRole(user.Id, administrator) == false)
+                userManager.AddToRole(user.Id, administrator);
         }
     }
 }
